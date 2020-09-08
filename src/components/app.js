6f4f7d1592590e88e5f23bfe2cda039bb6c5822c
RdzCore <daniel_rod_con@hotmail.com>
2020-09-07 19:40:03 -0500

    install moment.js, create clock component with lifecycles

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Home from './home';
+import Home from "./home";
+import Clock from "./clock";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <h1>TODO app :D</h1>
-        <Home/>
+        <Clock />
+        <Home />
       </div>
     );
   }
---
+++ src/components/clock.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import moment from "moment";
+
+export default class Clock extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dateNow: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.liveTime = setInterval(() => {
+      this.setState({
+        dateNow: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillMount() {
+      clearInterval(this.liveTime);
+  }
+
+  render() {
+    return (
+      <div className="clock-conteiner">
+        {moment(this.state.dateNow).format("MMMM Do YYYY, h:mm:ss a")}
+      </div>
+    );
+  }
+}
